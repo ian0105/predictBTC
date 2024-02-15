7d258684e99501b9cfb3f945663c7f59ac2bcf8c
eunchong-poza <prokkec@gmail.com>
2024-02-15 21:39:46 +0900

    수정

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,15 @@
 import numpy as np
 import torch
+from pytorch_lightning import Callback
+
+
+class GradNormCallback(Callback):
+    """
+    Callback to log the gradient norm.
+    """
+
+    def on_after_backward(self, trainer, model):
+        model.log("grad_norm", gradient_norm(model))
 def gradient_norm(model: torch.nn.Module, norm_type: float = 2.0) -> torch.Tensor:
     """
     Compute the gradient norm.
